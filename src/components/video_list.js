a36d0e739bee44c1885271650a0db02473aaaabf
Iurii Faevskii <urfaevskii@gmail.com>
2017-05-30 18:23:39 +0300

    Maping true data props in VideoList with key and create VideoItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
     //const videos = props.videos
-    const listItems = props.videos.map((item) =>
-    <li key={item.etag}>
-      {item.kind}
-    </li>
-    );
+
+    //example map!
+    //var array = [1,2,3];
+    //array.map(function(number){ return number *2 });
+
+    //in react we have JSX and we can 
+    //var array = [1,2,3];
+    //array.map(function(number){ return '<div>' + number + '</div>' });
+    //result: (3) ["<div>1</div>", "<div>2</div>", "<div>3</div>"]
+    const videoItems = props.videos.map((video)=> {
+        return <VideoListItem key={video.etag} video={video} />
+    });
+
     return (
         <ul className="col-md-4 list-group">
-            {props.videos.length}
-            {listItems}
+            {videoItems}
         </ul>
     );
 };
---
+++ src/components/video_list_item.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const VideoListItem = (props) => {
+    console.log('Video list item props:' + props)
+    return <li >video</li>
+};
+
+export default VideoListItem;
