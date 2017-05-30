b2c52e3ecc069bcaa9a92f6f9b86c7e712012b90
Iurii Faevskii <urfaevskii@gmail.com>
2017-05-30 16:33:33 +0300

    Create example 2 with state and Starting Value in input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,7 +6,7 @@ class SearchBar extends Component{
     constructor(props){
         super(props);
 
-        this.state = { term : ''}; //оголошує обєкт
+        this.state = { term : 'Starting Value'}; //оголошує обєкт
         console.log(this.state);
     }
     
@@ -14,11 +14,10 @@ class SearchBar extends Component{
         //this.setState({term: event.target.value}) 
         //те ж саме що, але так не правильно this.state.term = event.target.value
         return ( 
-            <div>
-                <input onChange = { event => this.setState({term: event.target.value})} />;
-                //змінює значення обєкта і присвоює йому значення інпута
-                value of input : {this.state.term}
-                // виводить на екран обєкт стейт
+            <div> {this.state.term}
+                <input 
+                value={this.state.term} 
+                onChange = { event => this.setState({term: event.target.value})} />
             </div>
         );
     }
